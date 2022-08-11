febc3001575e0edbd4d69782f359d604060a4e85
AhmedMahfouz <ahmelsamahy@gmail.com>
2022-08-12 01:10:21 +0200

    feat<modify.py> : add function to delete parent element with empty image tag
    image_link tag is empty after the link in it is moved to additional_image_link in the parent element before it, then the functions search for empty image_link and delete the parent that is relative to it

--- modify.py
+++ modify.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 
-mytree = ET.parse('202208112017.xml')
+mytree = ET.parse('feed.xml')
 myroot = mytree.getroot()
 dataRecord = myroot.findall("DATA_RECORD")
 recordLen = len(myroot.findall('DATA_RECORD'))
@@ -9,6 +9,7 @@ counterNeg = 0
 
 uniqueId = []
 
+# add empty additional_image_link element
 for allElem in myroot.findall('DATA_RECORD'):
     underIt = allElem.find('image_link')
     new_tag = ET.Element('additional_image_link')
@@ -18,20 +19,28 @@ for allElem in myroot.findall('DATA_RECORD'):
     allElem.insert(index+1, new_tag)
 
 for index in range(0, recordLen):
-    # print (myroot[index][4].text)
-    myroot[index][4].text = str("https://butopea.com/p/" + myroot[index][4].text)
-    if len(myroot[index][5].text)>2: 
-      myroot[index][5].text = str("https://butopea.com/p/" + myroot[index][5].text)
-    
-for index in range(0, recordLen):  
-  counter = 1
-  if counter+index < recordLen:
-      while (myroot[index][1].text == myroot[index+counter][1].text):
-          myroot[index][5].text = myroot[index][5].text + \
-              " \n " + myroot[index+counter][4].text
-          counter += 1
+    myroot[index][4].text = str(
+        "https://butopea.com/p/" + myroot[index][4].text)
+    if len(myroot[index][5].text) > 2:
+        myroot[index][5].text = str(
+            "https://butopea.com/p/" + myroot[index][5].text)
 
-# print(len(myroot.findall('DATA_RECORD')))
+
+# move the image link to the additional_image_link element if id are the same with the ones after it.
+for index in range(0, recordLen):
+    counter = 1
+    if counter+index < recordLen:
+        while (myroot[index][1].text == myroot[index+counter][1].text):
+            myroot[index][5].text = myroot[index][5].text + \
+                " \n " + myroot[index+counter][4].text
+            myroot[index+counter][4].text = " "
+            counter += 1
+    for target in myroot:
+        f_list= [t.text for t in target.findall('.//image_link')]
+        if " " in f_list:
+            myroot.remove(target)
+    recordLen = len(myroot.findall('DATA_RECORD'))
+
 
 # for allElem in myroot.findall('DATA_RECORD'):
 #     underIt = allElem.find('id')
@@ -65,13 +74,6 @@ for index in range(0, recordLen):
 #       links.text = str("https://butopea.com/p/"+(links.text))
 #       counter += 1
 # print("changed link for",counter, "product")
-# counter = 0
-
-# for links in myroot.iter('image_link'):
-#     # updates the price value
-#     links.text = str("https://butopea.com/"+(links.text))
-#     counter += 1
-# print("changed image_link for",counter, "product")
 # counter = 0
 
 mytree.write('output.xml')
